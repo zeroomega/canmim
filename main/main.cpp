8cc0dbf33035389ff3b8866cfc49e2ffae7352c8
Zero Omega <richardkszero@gmail.com>
2022-10-14 13:39:59 -0700

    Add oil pressure readings

--- main/main.cpp
+++ main/main.cpp
@@ -5,9 +5,11 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_log.h"
+#include "driver/adc.h"
 #include "driver/gpio.h"
 #include "driver/twai.h"
-#include "driver/adc.h"
+#include "driver/timer.h"
 #include "esp_adc_cal.h"
 #include "mcp2515.h"
 
@@ -17,12 +19,148 @@
 #define MCP_MISO  9
 #define MCP_CS    7
 
+// CAN Controller PIN Map
+#define CAN_TX    GPIO_NUM_3
+#define CAN_RX    GPIO_NUM_2
+
 // ADC PIN Map
-#define ADC_OILP  0
+#define ADC_OILP ADC1_CHANNEL_0
+
+// CAN ID for OIL Pressure
+#define OIL_PRESSURE_CAN_ID 0x662
+
+#define OIL_PRESSURE_R1 10
+#define OIL_PRESSURE_R2 22
+
+#define OIL_PRESSURE_VL 0.5f
+#define OIL_PRESSURE_VH 4.5f
+#define OIL_PRESSURE_PMAX 150
+
+// By default ESP32 timer is 80,000,000 Hz (80Mhz)
+// With divider of 800, the divider counter tick
+// every 0.000001 sec. 
+#define TIMER_DIVIDER TIMER_BASE_CLK / 100000
+
 
 // Global SPI Handle for MCP2515 Can Controller
 spi_device_handle_t spi_mcp2515;
 esp_adc_cal_characteristics_t adc1_chars;
+
+volatile bool shouldUpdateADC = false;
+
+uint32_t kPassthroughIDs[] = {
+    0x40,   // RPM, PPS
+    0x138,  // Steering Angle
+    0x139,  // Brake pressure.
+    0x345,  // OilTemp, ECT
+};
+
+// Test if CAN message should be pass through.
+bool IsPassthroughID(twai_message_t *message);
+
+// Helper function to cause system reboot when fatal error happens.
+void fatal_error();
+
+// Helper function to initiate ADC. Will reboot if error happens.
+void init_adc();
+
+// Helper function to initiate SPI Master controller. Will reboot if error
+// happens.
+void init_spi();
+
+// Helper function to initiate ESP32 CAN Controller.
+void init_can();
+
+void init_mcu();
+
+void init_timer();
+
+void update_adc();
+
+bool IRAM_ATTR timer_callback(void* arg);
+
+uint8_t get_normalized_oil_pressure();
+
+void send_oil_pressure_to_can(MCP2515 &mcpCan, uint8_t pressure);
+
+void handling_incoming_message(MCP2515&mcpCan);
+// Helper function to send received CAN message to the output CAN bus.
+// Errors will be ignored.
+void sendTWAIMessageToCan(MCP2515 &mcpCan, twai_message_t *twaiMessage);
+
+uint32_t adc_level1[10];
+uint32_t adc_level1_pointer = 0;
+uint32_t adc_level1_rolling_sum = 0;
+uint32_t adc_level2[3];
+uint32_t adc_level2_pointer = 0;
+uint32_t adc_level2_rolling_sum = 0;
+uint32_t adc_read_counter = 0;
+bool shouldSendOilPressure = false;
+uint32_t sampleCounter1 = 0;
+uint32_t sampleCounter2 = 0;
+
+extern "C" void app_main(void) {
+    init_mcu();
+    init_can();
+    init_spi();
+    printf("SPI Init\n");
+    MCP2515 mcpCan(&spi_mcp2515);
+    printf("Created MCP2515 object\n");
+    mcpCan.reset();
+    printf("Reset MCP2515 \n");
+    mcpCan.setBitrate(CAN_500KBPS, MCP_8MHZ);
+    printf("Set bitrate MCP2515 \n");
+    mcpCan.setOneShotMode(true);
+    printf("Set OneShot MCP2515 \n");
+    mcpCan.setNormalMode();
+    printf("Set Normal mode MCP2515 \n");
+    init_adc();
+    printf("ADC initialized\n");
+    init_timer();
+    printf("Timer initialized\n");
+   
+    
+    while(1) {
+        if (shouldUpdateADC) {
+            shouldUpdateADC = false;
+            update_adc();
+        }
+        if (adc_read_counter >= 5) {
+            adc_read_counter = 0;
+            uint8_t pressure = get_normalized_oil_pressure();
+            send_oil_pressure_to_can(mcpCan, pressure);
+        }
+        handling_incoming_message(mcpCan);
+    }
+}
+
+void init_mcu() {
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
+            CONFIG_IDF_TARGET,
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
+    memset(&adc_level1, 0, sizeof(adc_level1));
+    memset(&adc_level2, 0, sizeof(adc_level2));
+}
+
+bool IsPassthroughID(twai_message_t *message) {
+    for (int i = 0; i < sizeof(kPassthroughIDs)/sizeof(uint32_t); i++) {
+        if (kPassthroughIDs[i] == message->identifier)
+            return true;
+    }
+    return false;
+}
 
 void fatal_error() {
     for (int i = 10; i >= 0; i--) {
@@ -38,23 +176,23 @@ void init_adc() {
     esp_err_t ret;
     ret = esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP);
     if (ret != ESP_OK) {
-        printf("ADC Tow Point Calibration Data not available in FUSE %d\n", ret);
+        ESP_LOGE("ADC", "Tow Point Calibration Data not available in FUSE, code %d", ret);
         fatal_error();
     }
     esp_adc_cal_value_t cal_ret = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_11db, ADC_WIDTH_12Bit, 0, &adc1_chars);
     if (cal_ret == ESP_ADC_CAL_VAL_EFUSE_TP) {
-        printf("Calibrate data TP\n");
+        ESP_LOGW("ADC", "Calibration Data TP");
     } else {
-        printf("Calibrate data %d\n", cal_ret);
+        ESP_LOGW("ADC", "Calibration Data Code: %d", cal_ret);
     }
     ret = adc1_config_width(ADC_WIDTH_BIT_12);
     if (ret != ESP_OK) {
-        printf("ADC1 WIDTH Init failed %d\n", ret);
+        ESP_LOGE("ADC", "ADC1 WIDTH Init failed %s", esp_err_to_name(ret));
         fatal_error();
     }
-    ret = adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_11db);
+    ret = adc1_config_channel_atten(ADC_OILP, ADC_ATTEN_11db);
     if (ret != ESP_OK) {
-        printf("ADC1 ATTEN Init failed %d\n", ret);
+        ESP_LOGE("ADC", "ADC1 ATTEN Init failed %s", esp_err_to_name(ret));
         fatal_error();
     }
 }
@@ -72,21 +210,20 @@ void init_spi() {
     s_dev_config.mode = 0;
     s_dev_config.spics_io_num = MCP_CS;
     s_dev_config.queue_size = 1;
-    printf("Prepared 2 struts\n");
     esp_err_t ret_code = spi_bus_initialize(SPI2_HOST, &s_bus_config, SPI_DMA_CH_AUTO);
     if (ret_code != ESP_OK) {
-        printf("Failed to initialize SPI Bus, code %d\n", ret_code);
+        ESP_LOGE("SPI", "Failed to initialize SPI Bus, Error: %s", esp_err_to_name(ret_code));
         fatal_error();
     } else {
-        printf("SPI bus inited\n");
+        ESP_LOGW("SPI", "SPI bus initialized");
     }
 
     ret_code = spi_bus_add_device(SPI2_HOST, &s_dev_config, &spi_mcp2515);
     if (ret_code != ESP_OK) {
-        printf("Failed to initialize SPI Device, code %d\n", ret_code);
+        ESP_LOGE("SPI", "Failed to initialize SPI Device, Error: %s", esp_err_to_name(ret_code));
         fatal_error();
     } else {
-        printf("SPI device added\n");
+        ESP_LOGW("SPI", "SPI device added");
     }
 }
 
@@ -97,118 +234,141 @@ void sendTWAIMessageToCan(MCP2515 &mcpCan, twai_message_t *twaiMessage) {
     frame.can_dlc = twaiMessage->data_length_code;
     memcpy(frame.data, twaiMessage->data, 8);
     MCP2515::ERROR err = mcpCan.sendMessage(&frame);
-    if (err != MCP2515::ERROR_OK) {
-        printf("Failed sending to CAN %d\n", err);
-    } else {
-        printf("Can Message Sent\n");
-    }
+    if (err != MCP2515::ERROR_OK)
+        ESP_LOGE("MCPCAN", "Failed sending to CAN %d", err);
 }
 
 void init_can() {
     //Initialize configuration structures using macro initializers
-    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_3 /* TX */, GPIO_NUM_2 /* RX */, TWAI_MODE_LISTEN_ONLY);
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(CAN_TX /* TX */, CAN_RX /* RX */, TWAI_MODE_LISTEN_ONLY);
     twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
     twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
     //Install TWAI driver
     if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
-        printf("CAN bus Driver installed\n");
+        ESP_LOGW("ESPCAN", "Can bus driver installed");
     } else {
-        printf("Failed to install driver\n");
+        ESP_LOGE("ESPCAN", "Failed to install driver");
         fatal_error();
     }
     if (twai_start() == ESP_OK) {
-        printf("Driver started\n");
+        ESP_LOGW("ESPCAN", "Can bus started");
     } else {
-        printf("Failed to start driver\n");
+        ESP_LOGE("ESPCAN", "Failed to start");
         fatal_error();
     }
 }
 
-int rpm = 3500;
-twai_message_t getMessage() {
-    twai_message_t message;
-    message.identifier = 0x40;
-    if (rpm >=6000) {
-        rpm = 3500;
+void init_timer() {
+    timer_config_t timer_config;
+    memset(&timer_config, 0, sizeof(timer_config_t));
+    timer_config.alarm_en = TIMER_ALARM_EN;
+    timer_config.auto_reload = TIMER_AUTORELOAD_EN;
+    timer_config.counter_en = TIMER_PAUSE;
+    timer_config.counter_dir = TIMER_COUNT_UP;
+    timer_config.divider = TIMER_DIVIDER;
+    esp_err_t ret_code = timer_init(TIMER_GROUP_0, TIMER_0, &timer_config);
+    if (ret_code != ESP_OK) {
+        ESP_LOGE("TIMER", "Failed to initialize timer, error: %s", esp_err_to_name(ret_code));
+        fatal_error();
     }
-    message.data_length_code = 8;
-    
-    memset(&message.data, 0, 8);
-    message.data[2] = rpm & 0xff;
-    message.data[3] = (rpm & 0xff00)>>8;
-    return message;
+    // Alarm triggers every 0.001sec
+    ret_code = timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, 100);
+    if (ret_code != ESP_OK) {
+        ESP_LOGE("TIMER", "Failed to set alarm, error: %s", esp_err_to_name(ret_code));
+        fatal_error();
+    }
+    ret_code = timer_enable_intr(TIMER_GROUP_0, TIMER_0);
+    if (ret_code != ESP_OK) {
+        ESP_LOGE("TIMER", "Failed to enable interrupt, error: %s", esp_err_to_name(ret_code));
+        fatal_error();
+    }
+    ret_code = timer_isr_callback_add(TIMER_GROUP_0, TIMER_0, &timer_callback, nullptr, 0);
+    if (ret_code != ESP_OK) {
+        ESP_LOGE("TIMER", "Failed to add interrupt callback, error: %s", esp_err_to_name(ret_code));
+        fatal_error();
+    }
+    ret_code = timer_start(TIMER_GROUP_0, TIMER_0);
+    if (ret_code != ESP_OK) {
+        ESP_LOGE("TIMER", "Failed to start the timer, error: %s", esp_err_to_name(ret_code));
+        fatal_error();
+    }
 }
 
-extern "C" void app_main(void)
-{
-    printf("Hello world!\n");
+void update_adc() {
+    uint32_t raw = adc1_get_raw(ADC_OILP);
+    adc_read_counter++;
+    adc_level1_rolling_sum = adc_level1_rolling_sum - adc_level1[adc_level1_pointer] + raw;
+    adc_level1[adc_level1_pointer] = raw;
+    adc_level1_pointer++;
+    if (adc_level1_pointer == sizeof(adc_level1)/sizeof(uint32_t))
+        adc_level1_pointer = 0;
+    if (adc_level1_pointer != 0) {
+        uint32_t level2_raw = adc_level1_rolling_sum / (sizeof(adc_level1)/sizeof(uint32_t));
+        adc_level2_rolling_sum = adc_level2_rolling_sum - adc_level2[adc_level2_pointer] + level2_raw;
+        adc_level2[adc_level2_pointer] = level2_raw;
+        adc_level2_pointer++;
+        if (adc_level2_pointer == sizeof(adc_level2)/sizeof(uint32_t))
+            adc_level2_pointer = 0;
+    }
+}
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+uint8_t get_normalized_oil_pressure() {
+    uint32_t normalized_adc_raw = adc_level2_rolling_sum / (sizeof(adc_level2)/sizeof(uint32_t));
+    uint32_t mV = esp_adc_cal_raw_to_voltage(normalized_adc_raw, &adc1_chars);
+    float actualV = ((float)mV)/OIL_PRESSURE_R1*(OIL_PRESSURE_R1+OIL_PRESSURE_R2)/1000;
+    sampleCounter2++;
+    if (sampleCounter2 >= 1000) {
+        sampleCounter2  = 0;
+        ESP_LOGD("ADC", "Sample Voltage: %.2f", actualV);
+    }
+    if (actualV < OIL_PRESSURE_VL)
+        actualV = 0;
+    else
+        actualV -= OIL_PRESSURE_VL;
+    actualV = actualV * OIL_PRESSURE_PMAX / (OIL_PRESSURE_VH - OIL_PRESSURE_VL);
+    return (uint8_t)actualV;
+}
 
-    printf("silicon revision %d, ", chip_info.revision);
+bool IRAM_ATTR timer_callback(void* arg) {
+    shouldUpdateADC = true;
+    return false;
+}
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+void send_oil_pressure_to_can(MCP2515& mcpCan, uint8_t pressure) {
+    can_frame frame;
+    memset(&frame, 0, sizeof(can_frame));
+    frame.can_id = OIL_PRESSURE_CAN_ID;
+    frame.can_dlc = 8;
+    frame.data[0] = pressure;
+    MCP2515::ERROR err = mcpCan.sendMessage(&frame);
+    if (err != MCP2515::ERROR_OK)
+        ESP_LOGE("MCPCAN", "Failed sending oil pressure to CAN %d", err);
+}
 
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-    init_can();
-    init_spi();
-    printf("SPI Init\n");
-    MCP2515 mcpCan(&spi_mcp2515);
-    printf("Created MCP2515 object\n");
-    mcpCan.reset();
-    printf("Reset MCP2515 \n");
-    mcpCan.setBitrate(CAN_500KBPS, MCP_8MHZ);
-    printf("Set bitrate MCP2515 \n");
-    mcpCan.setOneShotMode(true);
-    printf("Set OneShot MCP2515 \n");
-    mcpCan.setNormalMode();
-    printf("Set Normal mode MCP2515 \n");
-    init_adc();
-   
-    
-    while(1) {
-        twai_message_t message;
-        esp_err_t ret_code;
-        ret_code = twai_receive(&message, pdMS_TO_TICKS(10000));
-        switch(ret_code) {
-            case ESP_OK:
-                printf("ID is %d\n", message.identifier);
-                if (!(message.rtr)) {
-                    for (int i = 0; i < message.data_length_code; i++) {
-                        printf("Data byte %d = %d\n", i, message.data[i]);
-                    }
-                    uint32_t adc_raw = adc1_get_raw(ADC1_CHANNEL_0);
-                    uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_raw, &adc1_chars);
-                    float converted = ((float)voltage)/10*(10+22)/1000;
-
-                    printf("Voltage: %.2f mv\n", converted);
-                    sendTWAIMessageToCan(mcpCan, &message);
+void handling_incoming_message(MCP2515&mcpCan) {
+    twai_message_t message;
+    esp_err_t ret_code;
+    ret_code = twai_receive(&message, pdMS_TO_TICKS(1));
+    switch(ret_code) {
+        case ESP_OK:
+            sampleCounter1++;
+            if (sampleCounter1 >= 1000) {
+                sampleCounter1 = 0;
+                ESP_LOGD("ESPCAN", "Sampling Message ID: %d", message.identifier);
+                for (int i = 0; i < message.data_length_code; i++) {
+                    ESP_LOGD("ESPCAN", "Data byte %d = %d", i, message.data[i]);
                 }
+            }
+            if (!IsPassthroughID(&message))
                 break;
-            case ESP_ERR_INVALID_ARG:
-                printf("Failed, Invalid Arg\n");
-                break;
-            case ESP_ERR_TIMEOUT:
-                printf("Failed, Time Out\n");
-                break;
-            case ESP_FAIL:
-                printf("Failed, ESP_FAIL\n");
-                break;
-            case ESP_ERR_INVALID_STATE:
-                printf("Failed, Invalid State\n");
-                break;
-            case ESP_ERR_NOT_SUPPORTED:
-                printf("Not supported\n");
-                break;
-        }
-        
-    }    
+            if (!(message.rtr))
+                sendTWAIMessageToCan(mcpCan, &message);
+            break;
+        case ESP_ERR_TIMEOUT:
+            // Timeout is normal when there is no CAN communication.
+            // Ignore it.
+            break;
+        default:
+            ESP_LOGE("ESPCAN", "Failed when receiving due to %s", esp_err_to_name(ret_code));
+    }
 }
